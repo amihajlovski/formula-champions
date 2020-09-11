4008be563365a7dd03a305eb1386a89e8d483e43
Aleksandar Mihajlovski <aleksandar.mihajlovski@polarcape.com>
2020-09-11 20:46:05 +0200

    added racing service

---
+++ src/app/services/racing-api-service.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { RacingApiServiceService } from './racing-api-service.service';
+
+describe('RacingApiServiceService', () => {
+  let service: RacingApiServiceService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(RacingApiServiceService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
---
+++ src/app/services/racing-api-service.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
+import { Response } from '../models/response';
+import { Standing } from '../models/standing';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RacingApiServiceService {
+  constructor(private http: HttpClient) {}
+
+  getDriverStandings(offset: number, limit: number): Observable<Standing[]> {
+    return this.http
+      .get<Response>(
+        `${environment.racingApiEndpoint}/driverStandings/1.json?offset=${offset}&limit=${limit}`
+      )
+      .pipe(map((data: Response) => data.MRData.StandingsTable.StandingsLists));
+  }
+}
