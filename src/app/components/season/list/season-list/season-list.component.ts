f015b838c55d1e1792bc0e46a0b642eaad2aa52d
Aleksandar Mihajlovski <aleksandar.mihajlovski@polarcape.com>
2020-09-12 15:28:32 +0200

    added season list component

---
+++ src/app/components/season/list/season-list/season-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { RaceTable } from 'src/app/models/race-table';
+
+@Component({
+  selector: 'app-season-list',
+  templateUrl: './season-list.component.html',
+  styleUrls: ['./season-list.component.scss'],
+})
+export class SeasonListComponent implements OnInit {
+  @Input()
+  season: string;
+
+  @Input()
+  data: RaceTable[];
+
+  columns = [];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
