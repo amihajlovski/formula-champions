b782b2b69de378b73ad8d6d8251ce18be912def2
Aleksandar Mihajlovski <aleksandar.mihajlovski@polarcape.com>
2020-09-15 09:20:43 +0200

    test timeline component

--- src/app/components/timeline/timeline.component.spec.ts
+++ src/app/components/timeline/timeline.component.spec.ts
@@ -1,25 +1,43 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
+import { driverStandings } from 'src/app/_mocks_/driverStandings';
 import { TimelineComponent } from './timeline.component';
 
 describe('TimelineComponent', () => {
   let component: TimelineComponent;
   let fixture: ComponentFixture<TimelineComponent>;
+  let timelineElement: DebugElement;
+  let timelineContentElement: Element;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ TimelineComponent ]
+  const standings = driverStandings.MRData.StandingsTable.StandingsLists;
+
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        imports: [RouterTestingModule],
+        declarations: [TimelineComponent],
+      }).compileComponents();
     })
-    .compileComponents();
-  });
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TimelineComponent);
     component = fixture.componentInstance;
+    component.championships = standings;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show a list of races', () => {
+    timelineElement = fixture.debugElement.query(By.css('section'));
+    timelineContentElement = timelineElement.nativeElement;
+    expect(
+      timelineContentElement.querySelectorAll('.timeline-content').length
+    ).toEqual(standings.length);
   });
 });
