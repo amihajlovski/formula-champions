e519f4c4a44d3a2acad8ec19d9cf2bb0abae0ae2
Aleksandar Mihajlovski <aleksandar.mihajlovski@polarcape.com>
2020-09-15 08:59:27 +0200

    fix test for getting standings

--- src/app/services/racing-api/racing-api.service.spec.ts
+++ src/app/services/racing-api/racing-api.service.spec.ts
@@ -3,6 +3,7 @@ import {
   HttpTestingController,
 } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { champion } from 'src/app/_mocks_/champion';
 import { driverStandings } from 'src/app/_mocks_/driverStandings';
 import { results } from 'src/app/_mocks_/results';
 import { environment } from 'src/environments/environment';
@@ -28,21 +29,42 @@ describe('RacingApiServiceService', () => {
   it('should get driver standings from 2005 to 2015', () => {
     const fromYear = 2005;
     const toYear = 2015;
+
     const offset = 55;
     const limit = 11;
 
     service.getDriverStandings(offset, limit).subscribe((data) => {
-      expect(data).toBeTruthy('No standings returned');
       expect(data).toBeTruthy();
 
-      const { 0: firstYear, [data.length - 1]: lastYear } = data;
+      const firstYear = data[0];
+      const lastYear = data[data.length - 1];
 
-      expect(firstYear.season).toContain(fromYear.toString());
-      expect(lastYear.season).toContain(toYear.toString());
+      expect(firstYear.season).toEqual(fromYear.toString());
+      expect(lastYear.season).toEqual(toYear.toString());
     });
 
     const req = httpTestingController.expectOne(
       `${environment.racingApiEndpoint}/driverStandings/1.json?offset=${offset}&limit=${limit}`
+    );
+    expect(req.request.method).toEqual('GET');
+  });
+
+  it('should get the champion for 2015', () => {
+    const year = 2015;
+
+    service.getChampionForYear(year).subscribe((data) => {
+      expect(data).toBeTruthy();
+
+      const { season, StandingsLists } = data;
+
+      expect(season).toEqual(year.toString());
+      expect(StandingsLists[0].DriverStandings[0].Driver.driverId).toEqual(
+        champion.driverId
+      );
+    });
+
+    const req = httpTestingController.expectOne(
+      `${environment.racingApiEndpoint}/${year}/driverStandings/1.json`
     );
     expect(req.request.method).toEqual('GET');
 
@@ -52,7 +74,6 @@ describe('RacingApiServiceService', () => {
   it('should get results for year', () => {
     const year = 2015;
     service.getResultsForYear(year).subscribe((data) => {
-      expect(data).toBeTruthy('No championships returned');
       expect(data.Races).toBeTruthy();
 
       const { season, position, Races } = data;
